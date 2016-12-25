7b50831e4d693bc1ea57ec826b20370512648ebb
Farshid Zaman <Fzee@Macs-Air.home>
2016-12-24 19:14:24 -0500

    Multiplayer functionality added

--- ghost/ghost.rb
+++ ghost/ghost.rb
@@ -1,46 +1,62 @@
 require "byebug"
 require "Set"
+require "colorize"
 
 class Game
-  attr_reader :fragment
-  attr_accessor :current_player, :previous_player
+  attr_accessor :fragment, :current_player, :previous_player
 
-  def initialize(player1, player2, dictionary)
-    @player1 = Player.new(player1)
-    @player2 = Player.new(player2)
+  def initialize(player_list, dictionary = "dictionary.txt")
+    @players_list = player_list.map { |name| Player.new(name)}
     @fragment = ""
-    @current_player = @player1
-    @previous_player = @player2
+    @current_player = @players_list[0]
+    @previous_player = @players_list[@players_list.length - 1]
     @dictionary = Set.new(File.readlines(dictionary).map!(&:chomp))
   end
 
   def play_round
-    until game_over? do
-      guessed_letter = current_player.guess
-      @fragment << guessed_letter
-        until is_in_dictionary(fragment) do
-          @fragment.chop
-          guessed_letter = current_player.alert_invalid_guess
-          @fragment << guessed_letter
-        end
-        puts "#{fragment}"
-      self.switch_players
+    until @previous_player.losses == 5
+      until round_over? do
+        guessed_letter = current_player.guess
+        @fragment << guessed_letter
+          until is_in_dictionary(fragment) do
+            @fragment.chop
+            guessed_letter = current_player.alert_invalid_guess
+            @fragment << guessed_letter
+          end
+          puts "#{fragment}"
+        self.switch_players
+      end
+      puts "Round over! #{previous_player.name} lost this round".red
+      @previous_player.losses += 1
+      @fragment = ""
     end
-    puts "Game over! Winner is #{current_player.name}"
+    puts "Game over! #{previous_player.name} has lost the game".red
   end
 
-  def game_over?
+  def round_over?
     @dictionary.include?(fragment)
   end
 
   def switch_players
-    if @current_player == @player1
-      @current_player = @player2
-      @previous_player = @player1
-    elsif @current_player == @player2
-      @current_player = @player1
-      @previous_player = @player2
+    current_player_index = @players_list.index(current_player)
+    @current_player = @players_list[(current_player_index + 1) % @players_list.length]
+    @previous_player = @players_list[current_player_index]
+  end
+
+  def self.start_game
+    more_players = true
+    players_list = Array.new
+    while more_players
+      puts "Please enter a player name".white
+      player_name = gets.chomp
+      players_list << player_name
+      puts "Enter 'Y' for more players and 'N' for no more players".green
+      command = gets.chomp
+      more_players = false if command != "y"
     end
+    game = Game.new(players_list)
+    game.play_round
+
   end
 
 
@@ -54,12 +70,15 @@ end
 
 class Player
   attr_reader :name
+  attr_accessor :wins, :losses
   def initialize(name)
     @name = name
+    @wins = 0
+    @losses = 0
   end
 
   def guess
-    puts "Hey #{name}, Please choose your letter"
+    puts "Hey #{name}, Please choose your letter".yellow
     guessed_letter = gets.chomp
     guessed_letter
   end
@@ -71,5 +90,5 @@ class Player
   end
 end
 
-game = Game.new("Saima", "Farshid", "dictionary.txt")
-game.play_round
+
+Game.start_game
